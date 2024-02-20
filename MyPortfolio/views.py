9051bc288fe67175fc3aeb173cd910b13984039a
Alvinah <42946374+r187483e@users.noreply.github.com>
2024-02-20 15:19:31 +0200

    initial commit

--- MyPortfolio/views.py
+++ MyPortfolio/views.py
@@ -1,9 +1,10 @@
 from django.contrib import messages
-from django.shortcuts import render
 from .models import Home, About, Profile, Category, Skills, Portfolio
 from django.core.mail import send_mail
-
-
+from django.http import HttpResponse
+from django.shortcuts import render
+from .forms import ContactForm
+from django.core.mail import send_mail
 
 def index(request):
     # Home
@@ -25,5 +26,23 @@ def index(request):
 
     return render(request, 'index.html', context)
 
-def simple_mail(request):
-    send_mail(message=f"{'message'}")
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+
+
+            send_mail(
+                'Message from Portfolio Contact Form',
+                f'Name: {name}\nEmail: {email}\n\nMessage:\n{message}',
+                email,  # sender's email address
+                ['zendielsingano@gmail.com'],  # recipient's email address
+                fail_silently=False,
+            )
+            return HttpResponse('Your message has been sent successfully.')
+    else:
+        form = ContactForm()
+    return render(request, 'index.html', {'form': form})
--- mysite/settings.py
+++ mysite/settings.py
@@ -139,4 +139,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'zendielsingano@gmail.com'
-EMAIL_HOST_PASSWORD = 'qpfizlssilpgagpj'
+EMAIL_HOST_PASSWORD = 'wfntupzcxzffswdt'
